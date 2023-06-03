92c661349831223e89a178e137a716e1d64eba9d
HV220 <diagenin@gmail.com>
2023-06-03 16:43:54 +0300

    redact

--- src/ex06/day09_ex06.sql
+++ src/ex06/day09_ex06.sql
@@ -1,78 +1,32 @@
-CREATE FUNCTION fnc_person_visits_and_eats_on_date(
-  IN pperson varchar DEFAULT 'Dmitriy', 
-  IN pprice numeric DEFAULT 500, IN pdate date DEFAULT '2022-01-08'
-) RETURNS TABLE (name varchar) AS $$ BEGIN RETURN QUERY (
-  (
-    SELECT 
-      pi.name 
-    FROM 
-      (
-        SELECT 
-          * 
-        FROM 
-          person_visits pv 
-        WHERE 
-          pv.visit_date = pdate
-      ) q1 
-      JOIN (
-        SELECT 
-          * 
-        FROM 
-          person 
-        WHERE 
-          person.name = pperson
-      ) q2 ON q1.person_id = q2.id 
-      JOIN pizzeria pi ON q1.pizzeria_id = pi.id 
-      JOIN (
-        SELECT 
-          * 
-        FROM 
-          menu 
-        WHERE 
-          menu.price < pprice
-      ) q3 ON pi.id = q3.pizzeria_id
-  ) 
-  INTERSECT 
-    (
-      SELECT 
-        pi.name 
-      FROM 
-        (
-          SELECT 
-            * 
-          FROM 
-            person_order po 
-          WHERE 
-            po.order_date = pdate
-        ) q1 
-        JOIN (
-          SELECT 
-            * 
-          FROM 
-            person 
-          WHERE 
-            person.name = pperson
-        ) q2 ON q1.person_id = q2.id 
-        JOIN (
-          SELECT 
-            * 
-          FROM 
-            menu 
-          WHERE 
-            menu.price < pprice
-        ) q3 ON q1.menu_id = q3.id 
-        JOIN pizzeria pi ON q3.pizzeria_id = pi.id
-    )
-);
+DROP FUNCTION fnc_person_visits_and_eats_on_date(varchar, numeric, date);
+
+CREATE FUNCTION fnc_person_visits_and_eats_on_date(IN pperson varchar DEFAULT 'Dmitriy',
+                                                   IN pprice numeric DEFAULT 500,
+                                                   IN pdate date DEFAULT '2022-01-08')
+RETURNS TABLE (name varchar)
+AS 
+$$
+BEGIN
+    RETURN QUERY (
+        SELECT DISTINCT pi.name
+        FROM person_visits pv
+            JOIN pizzeria pi ON pv.pizzeria_id = pi.id
+        WHERE pv.visit_date = pdate 
+              AND person_id IN (SELECT p.id 
+                                FROM person p
+                                WHERE p.name = pperson)
+        INTERSECT 
+        SELECT DISTINCT pi2.name
+        FROM menu m
+            JOIN pizzeria pi2 ON m.pizzeria_id = pi2.id
+        WHERE m.price < pprice
+    );
 END;
-$$ LANGUAGE plpgsql;
-SELECT 
-  * 
-FROM 
-  fnc_person_visits_and_eats_on_date(pprice := 800);
-SELECT 
-  * 
-FROM 
-  fnc_person_visits_and_eats_on_date(
-    pperson := 'Anna', pprice := 1300, pdate := '2022-01-01'
-  );
+$$
+LANGUAGE plpgsql;
+
+SELECT *
+FROM fnc_person_visits_and_eats_on_date(pprice := 800);
+
+SELECT *
+FROM fnc_person_visits_and_eats_on_date(pperson := 'Anna',pprice := 1300,pdate := '2022-01-01');
